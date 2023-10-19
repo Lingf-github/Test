8afa92ea7240a507934910322b8d4854d00fe671
Lingf-github <1060788284@qq.com>
2023-10-19 22:06:50 +0800

    第一次提交

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="app">
+          <div className="main">
+            <div className="touxiang"></div>
+            <button>Connect</button>
+            <div className="touxiang1"></div>
+          </div>
+      </div>
+  );
+}
+
+export default App;
+
+// import React from 'react';
+// import './App.css';
+// const image = require('./huli.png');
+// const image1 = require('./banner 1.png')
+// const image2 = require('./phantom-logo-freelogovectors 1.png')
+// function App() {
+//     return (
+//         <>
+//             <div className="app">
+//                 <div className="main-content">
+//                 </div>
+//                 <div className="sidebar">
+//                     <div className="touxiang"></div>
+//                     <button>Connect</button>
+//                     <div className="touxiang1"></div>
+//                 </div>
+//             </div>
+//             <div className="center">
+//                 <img src={image}  alt="" className="huli"/>
+//             </div>
+//             <div><img src={image1} alt="" className="banner" /></div>
+//             <div><img src={image2} alt="" className="phantom" /></div>
+//         </>
+//     );
+// }
+//
+// export default App;
